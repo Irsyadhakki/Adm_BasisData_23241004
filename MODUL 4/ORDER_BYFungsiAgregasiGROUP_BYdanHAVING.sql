b3c2e6c50f66bc863d20417075584e0e4b2b8c86
Irsyad Hakki <145946160+Irsyadhakki@users.noreply.github.com>
2025-06-23 10:58:28 +0800

    Add files via upload

--- MODUL 4/ORDER_BYFungsiAgregasiGROUP_BYdanHAVING.sql
+++ MODUL 4/ORDER_BYFungsiAgregasiGROUP_BYdanHAVING.sql
@@ -2,7 +2,7 @@ USE pti_mart;
 
 -- CREATE DATABASES pti_mart;
 
--- nama : irsyad hakki
+-- nama : Irsyad hakki
 -- nim : 23241004
 -- kelas : A/4
 
@@ -64,6 +64,60 @@ ORDER BY harga DESC;
 SELECT nama_produk, qty, harga FROM tr_penjualan_dqlab
 WHERE harga >= 100000 OR nama_produk LIKE 'T%'
 ORDER BY diskon_persen DESC;
+
+
+--- FUNGSI AGREGASI ---
+-- SUM (menjumlahkan nilai)
+SELECT SUM(qty) AS total_qty FROM tr_penjualan_dqlab;
+
+-- praktek 8
+-- COUNT (menghitung jumlah baris)
+SELECT COUNT(*) AS jumlah_baris FROM tr_penjualan_dqlab;
+SELECT * FROM tr_penjualan_dqlab;
+
+-- praktek 9
+-- menggunakan 2 fungsi agregasi (sum dan count)
+SELECT SUM(qty) AS total_qty, COUNT(*) AS jumlah_baris FROM tr_penjualan_dqlab;
+
+-- praktek 10
+SELECT AVG(qty) AS rata_rata, MAX(qty) AS maximum, MIN(qty) AS minimum FROM tr_penjualan_dqlab;
+
+-- praktek 11
+-- COUNT (DISTINC)
+-- tanpa count(distinc)
+SELECT COUNT(nama_produk) FROM tr_penjualan_dqlab;
+
+-- dengan count(distinc)
+SELECT COUNT(DISTINCT nama_produk) FROM tr_penjualan_dqlab;
+
+-- praktek 12
+SELECT COUNT(DISTINCT nama_produk), COUNT(nama_produk) FROM tr_penjualan_dqlab;
+
+-- praktek 13
+SELECT nama_produk, MAX(qty) FROM tr_penjualan_dqlab;
+
+-- praktek 14
+-- group by (pengelompokan isi data dari 1 kolom atau lebih dari biasanya digabung dengan fungsi agregasi)
+-- ambil nilai dari pengelompokan nama produk dari tabel penjualan
+SELECT nama_produk FROM tr_penjualan_dqlab GROUP BY nama_produk;
+
+-- praktek 15
+-- penggunaan group by dengan 2 kolom
+SELECT nama_produk, qty FROM tr_penjualan_dqlab GROUP BY nama_produk, qty;
+
+-- praktek 16
+-- group by + agregasi
+SELECT nama_produk, SUM(qty) FROM tr_penjualan_dqlab GROUP BY nama_produk;
+
+-- praktek 17
+-- group by + agregasi + order by
+SELECT nama_produk, SUM(qty) 
+FROM tr_penjualan_dqlab 
+GROUP BY nama_produk 
+ORDER BY SUM(qty) DESC;
+select * from tr_penjualan_dqlab;
+SELECT DATEDIFF(NOW(),tgl_transaksi) from tr_penjualan_dqlab order by DATEDIFF(NOW(),tgl_transaksi) desc;
+      
  
 
 
@@ -75,3 +129,4 @@ ORDER BY diskon_persen DESC;
 
 
 
+DESCRIBE ms_pelanggan_dqlab;
